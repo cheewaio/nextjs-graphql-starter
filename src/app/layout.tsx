6240f1deb463b69f00b18d22666ba256920b17d6
Chee Wai Ooi <cheewaio@hotmail.com>
2026-08-26 12:57:07 -0400

    update theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,9 @@
 import type { Metadata } from "next";
-import { Plus_Jakarta_Sans, JetBrains_Mono } from "next/font/google";
+import { Plus_Jakarta_Sans, JetBrains_Mono, Raleway } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
 
-const sans = Plus_Jakarta_Sans({
-  subsets: ["latin"],
-  variable: "--font-sans-family",
-  display: "swap",
-});
+const raleway = Raleway({subsets:['latin'],variable:'--font-sans'});
 
 const mono = JetBrains_Mono({
   subsets: ["latin"],
@@ -27,7 +24,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${sans.variable} ${mono.variable} h-full antialiased`}
+      className={cn("h-full", "antialiased", mono.variable, "font-sans", raleway.variable)}
     >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
